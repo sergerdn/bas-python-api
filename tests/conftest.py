63a1b78a03cd95182b8320a9de6ebc21a5e9c58f
sergerdn <64213648+sergerdn@users.noreply.github.com>
2022-09-14 15:53:55 +0000

    feat: add tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,7 +3,7 @@ import os
 import pytest
 from dotenv import load_dotenv
 
-from tests import ABS_PATH
+from tests import ABS_PATH, DATA_DIR
 
 dotenv_path = os.path.join(ABS_PATH, ".env")
 if os.path.exists(dotenv_path):
@@ -23,3 +23,11 @@ def remote_script_user():
 @pytest.fixture(scope="module")
 def remote_script_password():
     return os.environ.get("TEST_REMOTE_SCRIPT_PASSWORD")
+
+
+@pytest.fixture(scope="module")
+def working_dir():
+    if not os.path.exists:
+        os.makedirs(DATA_DIR)
+
+    return DATA_DIR
--- tests/functional/test_api_basic.py
+++ tests/functional/test_api_basic.py
@@ -1,18 +1,23 @@
+import pytest
+
 from bas_api import BasApi, RemoteTransportOptions
 
 
-def test_api_basic_env_set(remote_script_name, remote_script_user, remote_script_password):
+def test_api_basic_env_set(remote_script_name, remote_script_user, remote_script_password, working_dir):
     assert remote_script_name is not None
     assert remote_script_user is not None
     assert remote_script_password is not None
+    assert working_dir is not None
 
 
 class TestApiBasic:
-    async def test_api_basic(self, remote_script_name, remote_script_user, remote_script_password):
+    @pytest.mark.asyncio
+    async def test_api_basic(self, remote_script_name, remote_script_user, remote_script_password, working_dir):
         transport_options = RemoteTransportOptions(
             remote_script_name=remote_script_name,
             remote_script_user=remote_script_user,
             remote_script_password=remote_script_password,
+            working_dir=working_dir,
         )
         api = BasApi(transport_options=transport_options)
         await api.set_up()
