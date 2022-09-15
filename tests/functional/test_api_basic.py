ee2da9146f59ff9e66dbf1b215697643b662b8af
sergerdn <64213648+sergerdn@users.noreply.github.com>
2022-09-15 11:44:33 +0000

    feat: improve tests

--- tests/functional/test_api_basic.py
+++ tests/functional/test_api_basic.py
@@ -21,6 +21,15 @@ async def clean_dir(dir_path):
     return True
 
 
+@pytest.mark.dependency()
+def test_api_basic_env_set(transport_options):
+    assert transport_options.remote_script_name is not None
+    assert transport_options.remote_script_user is not None
+    assert transport_options.remote_script_password is not None
+    assert transport_options.working_dir is not None
+    assert os.path.exists(transport_options.working_dir) is True
+
+
 @pytest.mark.dependency(depends=["test_api_basic_env_set"])
 @pytest.mark.asyncio
 class TestApiBasic:
