ee5a5bae1cd959c4ad4711105ef4179de6b4ba0a
sergerdn <64213648+sergerdn@users.noreply.github.com>
2022-09-17 13:30:30 +0000

    feat: bump version

--- bas_client/__init__.py
+++ bas_client/__init__.py
@@ -1,3 +1,4 @@
+__version__ = "0.1.2"
 from bas_client.client import BasClient, BasClientSettings, BrowserOptions, RemoteTransportOptions
 
 __all__ = ["BasClient", "BasClientSettings", "BrowserOptions", "RemoteTransportOptions"]
