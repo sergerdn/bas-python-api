0584841acc00f335a832af3a1d1b7e9f6d318756
sergerdn <64213648+sergerdn@users.noreply.github.com>
2022-09-20 13:52:24 +0000

    feat: add loggers

--- tests/functional/test_bas_client.py
+++ tests/functional/test_bas_client.py
@@ -1,4 +1,3 @@
-import asyncio
 import os.path
 import random
 
@@ -30,7 +29,7 @@ class TestBasClient:
         await clean_dir_async(browser_options.profile_folder_path)
         assert os.path.exists(browser_options.profile_folder_path) is False
 
-    async def test_client_browser_profile_dir_new(self, transport_options, google_url):
+    async def test_client_browser_profile_created(self, transport_options, google_url):
         """
         Extended settings to bas client: custom profile folder for new profile.
 
@@ -54,12 +53,10 @@ class TestBasClient:
 
         await client.browser.close()
         await client.clean_up()
+        """ closed profile"""
+        assert not os.path.isfile(os.path.join(profile_folder_path, "lockfile"))
 
-        browser_options = client.browser.options_get()
-        await clean_dir_async(browser_options.profile_folder_path)
-        assert os.path.exists(browser_options.profile_folder_path) is False
-
-    async def test_client_browser_profile_dir_old(self, transport_options, google_url):
+    async def test_client_browser_profile_old_used(self, transport_options, google_url):
         """
         Extended settings to bas client: custom profile folder for existing profile.
         :param transport_options:
@@ -74,24 +71,14 @@ class TestBasClient:
         await client.browser.load(google_url)
         await client.waiters.wait_full_page_load()
         cookies_first_obj = await client.network.save_cookies()
-        await client.clean_up()
 
         browser_options = client.browser.options_get()
         profile_folder_path = browser_options.profile_folder_path
+        await client.clean_up()
 
         assert os.path.exists(profile_folder_path) is True
 
-        # wait for browser closed
-        while os.path.exists(os.path.join(profile_folder_path, "lockfile")):
-            await asyncio.sleep(0.5)
         """using old profile"""
-
-        bas_api_settings = BasClientSettings(working_dir=transport_options.working_dir)
-        browser_options = BrowserOptions(profile_folder_path=profile_folder_path)
-
-        client = BasClient(
-            transport_options=transport_options, bas_client_settings=bas_api_settings, browser_options=browser_options
-        )
         await client.set_up()
 
         await client.browser.load("https://duckduckgo.com/")
@@ -101,13 +88,6 @@ class TestBasClient:
         # old profile loaded
         assert os.path.exists(os.path.join(profile_folder_path, "lockfile"))
         await client.clean_up()
-
-        # wait for browser closed
-        while os.path.exists(os.path.join(profile_folder_path, "lockfile")):
-            await asyncio.sleep(0.5)
-
-        await clean_dir_async(profile_folder_path)
-        assert os.path.exists(profile_folder_path) is False
 
         # old cookies exists
         for one in cookies_first_obj.cookies:
