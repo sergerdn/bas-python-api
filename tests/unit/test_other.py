6d1f9adcb0397f6a57d2d2e70668e152ab26c6f7
sergerdn <64213648+sergerdn@users.noreply.github.com>
2022-09-23 08:53:33 +0000

    feat: improve browser

--- tests/unit/test_other.py
+++ tests/unit/test_other.py
@@ -4,5 +4,9 @@ from bas_client.browser.exceptions import BrowserTimeout
 def test_docstring_message_exception():
     exc = BrowserTimeout()
     assert str(exc) == BrowserTimeout.__doc__
+
     exc = BrowserTimeout("some message")
     assert str(exc) == "some message"
+
+    exc = BrowserTimeout()
+    assert str(exc) == BrowserTimeout.__doc__
